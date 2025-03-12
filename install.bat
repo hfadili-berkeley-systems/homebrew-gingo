70eba2d5858f357ff6748b6aa2c18e2c648eb30d
Hicham Fadili <39830853+hfadili-berkeley-systems@users.noreply.github.com>
2025-03-12 14:50:18 +0000

    installl windows

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+curl -L -o gingo.exe https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.7/gingo-windows.exe
+mkdir "C:\Program Files\Gingo"
+move gingo.exe "C:\Program Files\Gingo\"
+setx PATH "%PATH%;C:\Program Files\Gingo"
+echo Installation Complete! Restart your terminal and run gingo.exe version
--- install.sh
+++ install.sh
@@ -8,21 +8,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     FILE="gingo-mac-amd64"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     FILE="gingo-linux"
-elif [[ "$OS" == "Windows_NT" ]]; then
-    FILE="gingo-windows.exe"
 else
     echo "Unsupported OS"
     exit 1
-fi
-
-# Windows Installation
-if [[ "$OS" == "Windows_NT" ]]; then
-    echo "Downloading Gingo for Windows..."
-    curl -L -o gingo.exe "$GITHUB_URL/$FILE"
-
-    echo "Installation complete! Run 'gingo.exe --version' to check."
-    echo "Note: You may need to add the file to your system PATH manually."
-    exit 0
 fi
 
 # macOS/Linux Installation
