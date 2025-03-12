33a1494b79683ebd6cf7fc6e4ff37d1abf1d68d8
Hicham Fadili <39830853+hfadili-berkeley-systems@users.noreply.github.com>
2025-03-12 13:05:35 +0000

    update formula

--- Formula/gingo.rb
+++ Formula/gingo.rb
@@ -4,10 +4,10 @@ class Gingo < Formula
     version "1.0.0"
   
     if OS.mac?
-      url "https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.0/gingo-mac-amd64"
+      url "https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.5/gingo-mac-amd64"
       sha256 "c2cdd4f118508a1a7cfe009a985aa86399964cc736cd176d18120f1a88e24198"
     elsif OS.linux?
-      url "https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.0/gingo-linux"
+      url "https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.5/gingo-linux"
       sha256 "07b70e4b18aa37490aac50f74829b1defe111ab967c6f53fd00808b697dcef70"
     end
   
