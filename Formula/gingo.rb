43677384c0619f4d276c18b4c3dfce2d42f9da10
Hicham Fadili <39830853+hfadili-berkeley-systems@users.noreply.github.com>
2025-03-12 13:03:00 +0000

    update formula

--- Formula/gingo.rb
+++ Formula/gingo.rb
@@ -1,8 +1,8 @@
-class gingo < Formula
-    desc "Gingo a simple and efficient CLI tool for generating a structured Go project layout. Whether you're starting a new project or looking to adopt best practices in structuring your Go code, Gingo provides a clean and maintainable foundation."
+class Gingo < Formula
+    desc "Gingo - A simple and efficient CLI tool for generating a structured Go project layout."
     homepage "https://github.com/hfadili-berkeley-systems/homebrew-gingo"
     version "1.0.0"
-    gingo.
+  
     if OS.mac?
       url "https://github.com/hfadili-berkeley-systems/homebrew-gingo/releases/download/v1.0.0/gingo-mac-amd64"
       sha256 "c2cdd4f118508a1a7cfe009a985aa86399964cc736cd176d18120f1a88e24198"
@@ -12,8 +12,11 @@ class gingo < Formula
     end
   
     def install
-      bin.install "gingo-mac-amd64" => "gingo" if OS.mac?
-      bin.install "gingo-linux" => "gingo" if OS.linux?
+      if OS.mac?
+        bin.install "gingo-mac-amd64" => "gingo"
+      elsif OS.linux?
+        bin.install "gingo-linux" => "gingo"
+      end
     end
   
     test do
